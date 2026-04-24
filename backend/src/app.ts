4c96531a312b0e5f8b3b0c23d316a2f1b1323b2b
Shoaib Ali <sali18089@gmail.com>
2026-04-24 15:31:23 +0500

    Added basic routes setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,19 @@
 import express from "express";
+import authRoutes from "./routes/authRoutes"
+import chatRoutes from "./routes/chatRoutes"
+import messageRoutes from "./routes/messageRoutes"
+import userRoutes from "./routes/userRoutes"
 
 const app = express();
+
+app.get("/health" , (req,res)=> {
+    res.json({status:"ok", message:"Server is running"})
+
+})
+
+app.use("/api/auth", authRoutes)
+app.use("/api/chats", chatRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/user", userRoutes)
 
 export default app;

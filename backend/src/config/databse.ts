bcdc407c3bdcc20ebdc5e99dd43b81b7ea8f1715
Shoaib Ali <sali18089@gmail.com>
2026-04-24 15:59:39 +0500

    Added check for mongo env variable

--- backend/src/config/databse.ts
+++ backend/src/config/databse.ts
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    const mongoUri = process.env.MONGODB_URI;
+    if (!mongoUri) {
+      throw new Error("MONGODB_URI env is not deifned");
+    }
+    await mongoose.connect(mongoUri as string);
     console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("MongoDB connection error: ", error)
+    console.error("MongoDB connection error: ", error);
     process.exit(1);
   }
 };

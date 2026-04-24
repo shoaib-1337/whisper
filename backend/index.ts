963a80fc72706b1be14f4da2028ce88980d4d7c0
Shoaib Ali <sali18089@gmail.com>
2026-04-24 15:22:29 +0500

    Express app setup and db connection

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import app from "./src/app";
+import { connectDB } from "./src/config/databse";
+
+const PORT = process.env.PORT || 3000;
+
+connectDB().then(() => {
+    app.listen(PORT,()=> {
+        console.log(`Server is running at port ${PORT}`);
+        
+    })
+});

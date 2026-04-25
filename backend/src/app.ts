c22c016f31c78d06de8ef2498cd5cf145558cd4e
Shoaib Ali <sali18089@gmail.com>
2026-04-25 14:58:34 +0500

    Clerk auth backend setup and auth middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,23 @@
 import express from "express";
-import authRoutes from "./routes/authRoutes"
-import chatRoutes from "./routes/chatRoutes"
-import messageRoutes from "./routes/messageRoutes"
-import userRoutes from "./routes/userRoutes"
+import { clerkMiddleware } from "@clerk/express";
+import authRoutes from "./routes/authRoutes";
+import chatRoutes from "./routes/chatRoutes";
+import messageRoutes from "./routes/messageRoutes";
+import userRoutes from "./routes/userRoutes";
 
 const app = express();
 
+app.use(clerkMiddleware());
+
 app.use(express.json());
 
-app.get("/health" , (req,res)=> {
-    res.json({status:"ok", message:"Server is running"})
+app.get("/health", (req, res) => {
+  res.json({ status: "ok", message: "Server is running" });
+});
 
-})
-
-app.use("/api/auth", authRoutes)
-app.use("/api/chats", chatRoutes)
-app.use("/api/messages", messageRoutes)
-app.use("/api/user", userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/chats", chatRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/user", userRoutes);
 
 export default app;
---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,30 @@
+import type { Request, Response, NextFunction } from "express";
+
+import { getAuth } from "@clerk/express";
+import { User } from "../models/User";
+import { requireAuth } from "@clerk/express";
+
+export type AuthReq = Request & {
+  userId?: string;
+};
+
+export const protectRoute = [
+  requireAuth(),
+  async (req: AuthReq, res: Response, next: NextFunction) => {
+    try {
+      const { userId: clerkId } = getAuth(req);
+      if (!clerkId) {
+        return res.status(401).json({ message: "Unautorized - invalid token" });
+      }
+      const user = await User.findOne({ clerkId });
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      req.userId = user._id.toString();
+      next();
+    } catch (error) {
+      console.error("Error in protectRoute middleware");
+      res.status(500).json({ message: "Internal Server error" });
+    }
+  },
+];

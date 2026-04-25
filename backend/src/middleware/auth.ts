2caf0995b86f926009fcc43908912003d5226d4a
Shoaib Ali <sali18089@gmail.com>
2026-04-25 15:27:24 +0500

    Resolved codeRabbit reviews

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -13,9 +13,9 @@ export const protectRoute = [
   async (req: AuthReq, res: Response, next: NextFunction) => {
     try {
       const { userId: clerkId } = getAuth(req);
-      if (!clerkId) {
-        return res.status(401).json({ message: "Unautorized - invalid token" });
-      }
+      //   if (!clerkId) {
+      //     return res.status(401).json({ message: "Unautorized - invalid token" });
+      //   }
       const user = await User.findOne({ clerkId });
       if (!user) {
         return res.status(404).json({ message: "User not found" });
@@ -23,8 +23,8 @@ export const protectRoute = [
       req.userId = user._id.toString();
       next();
     } catch (error) {
-      console.error("Error in protectRoute middleware");
-      res.status(500).json({ message: "Internal Server error" });
+      res.status(500);
+      next(error);
     }
   },
 ];

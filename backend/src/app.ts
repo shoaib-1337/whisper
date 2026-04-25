1383ddebfb8de02378d0b3785e2bbd83ee177c80
Shoaib Ali <sali18089@gmail.com>
2026-04-25 15:21:18 +0500

    Added ErrorHandler middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import authRoutes from "./routes/authRoutes";
 import chatRoutes from "./routes/chatRoutes";
 import messageRoutes from "./routes/messageRoutes";
 import userRoutes from "./routes/userRoutes";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
@@ -19,5 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/chats", chatRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/user", userRoutes);
+
+app.use(errorHandler);
 
 export default app;

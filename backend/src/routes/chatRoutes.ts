2fc588ab09b09d4a7472e9a5f40b9822256ad9d4
Shoaib Ali <sali18089@gmail.com>
2026-04-27 14:39:06 +0500

    Added chat routes

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth";
+import { getChats, getOrCreateChat } from "../contollers/chatController";
 
-const router = Router()
+const router = Router();
+
+router.get("/", protectRoute, getChats);
+router.get("/with/:participantId", protectRoute, getOrCreateChat);
 
 export default router;

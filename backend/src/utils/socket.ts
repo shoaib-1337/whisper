a798405bbb20e38039b4982937572699f95cc1e1
Shoaib Ali <sali18089@gmail.com>
2026-04-27 15:19:20 +0500

    Added socket auth verifiaction

--- backend/src/utils/socket.ts
+++ backend/src/utils/socket.ts
@@ -6,4 +6,34 @@ import { Message } from "../models/Message";
 import { User } from "../models/User";
 import { Chat } from "../models/Chat";
 
-export const initializeSocket = (httpServer: HttpServer) => {};
+interface SocketWithUserId extends Socket {
+  userId: string;
+}
+
+export const initializeSocket = (httpServer: HttpServer) => {
+  const allowedOrigins = [
+    "http://localhost:8081",
+    "http://localhost:5173",
+    process.env.FRONTEND_URL as string,
+  ];
+  const io = new SocketServer(httpServer, {
+    cors: { origin: allowedOrigins },
+  });
+
+  io.use(async (socket, next) => {
+    const token = socket.handshake.auth.token;
+    if (!token) return next(new Error("Authentication error"));
+    try {
+      const session = await verifyToken(token, {
+        secretKey: process.env.CLERK_SECRET_KEY,
+      });
+      const clerkId = session.sub;
+      const user = await User.findOne({ clerkId });
+      if (!user) return next(new Error("User not found"));
+      (socket as SocketWithUserId).userId = user._id.toString();
+      next();
+    } catch (error) {
+      next(new Error("Auth Error"));
+    }
+  });
+};
